d7322e13962de783e065b14d10ed96e522748956
jiangshouzhuang <jiangshouzhuang@163.com>
2016-09-16 14:14:31 +0800

    Update run-wordcount.sh

--- config/run-wordcount.sh
+++ config/run-wordcount.sh
@@ -8,22 +8,22 @@ echo "Hello Docker" >input/file2.txt
 echo "Hello Hadoop" >input/file1.txt
 
 # create input directory on HDFS
-hadoop fs -mkdir -p input
+hadoop fs -mkdir -p /input
 
 # put input files to HDFS
-hdfs dfs -put ./input/* input
+hdfs dfs -put ./input/* /input
 
 # run wordcount 
-hadoop jar $HADOOP_HOME/share/hadoop/mapreduce/sources/hadoop-mapreduce-examples-2.7.2-sources.jar org.apache.hadoop.examples.WordCount input output
+hadoop jar $HADOOP_HOME/share/hadoop/mapreduce/sources/hadoop-mapreduce-examples-2.7.3-sources.jar org.apache.hadoop.examples.WordCount /input /output
 
 # print the input files
 echo -e "\ninput file1.txt:"
-hdfs dfs -cat input/file1.txt
+hdfs dfs -cat /input/file1.txt
 
 echo -e "\ninput file2.txt:"
-hdfs dfs -cat input/file2.txt
+hdfs dfs -cat /input/file2.txt
 
 # print the output of wordcount
 echo -e "\nwordcount output:"
-hdfs dfs -cat output/part-r-00000
+hdfs dfs -cat /output/part-r-00000
 
