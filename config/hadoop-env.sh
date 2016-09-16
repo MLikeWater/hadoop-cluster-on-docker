44b0a3c1a6b94fc95e487fba81a469affc1d129e
jiangshouzhuang <jiangshouzhuang@163.com>
2016-09-16 13:50:55 +0800

    Update hadoop-env.sh

--- config/hadoop-env.sh
+++ config/hadoop-env.sh
@@ -22,7 +22,7 @@
 # remote nodes.
 
 # The java implementation to use.
-export JAVA_HOME=/usr/lib/jvm/java-7-oracle
+export JAVA_HOME=/usr/lib/jvm/default
 
 # The jsvc implementation to use. Jsvc is required to run secure datanodes
 # that bind to privileged ports to provide authentication of data transfer
